506c6f40acf09b8b549c1b0fff5dcf1363adc101
Pete Brooks <pete.brooks@tablexi.com>
2017-01-24 19:59:47 -0600

    Add secret config file

--- config/config.exs
+++ config/config.exs
@@ -21,10 +21,8 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+config :oed,
+  app_key: "APP_KEY",
+  app_id: "APP_ID"
+
+if File.exists?("config/secret.exs"), do: import_config "secret.exs"
